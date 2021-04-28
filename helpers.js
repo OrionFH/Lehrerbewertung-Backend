1be2aa6a013e935b9c59a475c0e23467ec08a9e8
OrionFH <maximilian.guettinger@t-online.de>
2021-04-28 12:35:03 +0200

    Added config file and generator for Top 10 list

--- helpers.js
+++ helpers.js
@@ -1,26 +1,41 @@
 var mysql = require('mysql');
+var conf = require('config');
 const restifyJsonSchemaValidationMiddleware = require('restify-json-schema-validation-middleware');
-var pool  = mysql.createPool({
-    connectionLimit : 100,
-    host            : 'localhost',
-    database        : 'standard'
-});
+var pool  = mysql.createPool(Object.assign({connectionLimit: 100}, conf.get("MYSQL")));
 
 const { v4: uuidv4 } = require('uuid');
 
 var jwt = require("jsonwebtoken");
 
+//NEVER PUBLISH THIS TO GitHub NEVER
+var secret = conf.get("TOKEN_SECRET");
 
 var genAccessToken = function(databaseRes) {
     return jwt.sign({
-        isModerator: databaseRes.moderator == 1,
-        isStudent: databaseRes.jahrgangsstufe != 0,
         userID: databaseRes.id
     }, secret, { expiresIn: '30m' });
 };
 
 module.exports = {
     pool,
+    checkAccessToken: function(accessToken, callback) {
+        jwt.verify(accessToken, secret, function(err, decoded) {
+            if(err){
+                callback(null);
+            }
+            else {
+                pool.query('select id, jahrgangsstufe, nick, moderator, pwhash from accounts where accounts.id = ?;', [decoded.userID], function (err, result, fields) {
+                    if (err) throw err;
+                    if(result.length === 1){
+                        callback(result[0]);
+                    }
+                    else {
+                        callback(null);
+                    }
+                });
+            }
+        });
+    },
     checkAccountValidation: function(hash, callback){
         pool.query('select id, jahrgangsstufe, nick, moderator from accounts where pwhash=' + mysql.escape(hash) + ";", function (err, result, fields) {
             if (err) throw err;
@@ -31,6 +46,44 @@ module.exports = {
                 callback(null);
             }
         });
+    },
+    genAccessTokenFromHash: function(hash, callback) {
+        this.checkAccountValidation(hash, function(res){
+            if(res != null) {
+                var accessToken = genAccessToken(res);
+                uuid = uuidv4();
+                
+                pool.query('UPDATE accounts SET accounts.refreshToken = ? WHERE accounts.pwhash = ?;', [uuid, hash], function(err, result, fields) {  
+                    if (err) throw err;                
+                    var refreshToken = jwt.sign({
+                        jti: uuid
+                    }, secret, { expiresIn: '200d'});
+                    callback(accessToken, refreshToken, res);
+                });
+            }
+            else {
+                callback(null);
+            }
+        })
+    },
+    genAccessTokenFromRefresh: function(refreshToken, callback) {
+        jwt.verify(refreshToken, secret, function(err, decoded) {
+            if(err) {
+                callback(null);
+            }
+            else {
+                jti = decoded.jti;
+                pool.query('select id, jahrgangsstufe, nick, moderator from accounts where refreshToken = ?;', [jti], function (err, result, fields) {
+                    if (err) throw err;
+                    if(result.length === 1){
+                        callback(genAccessToken(result[0]));
+                    }
+                    else {
+                        callback(null);
+                    }
+                });
+            }
+        });        
     },
     isCreator: function(pwhash, reviewID, callback){
         pool.query('select reviews.id from reviews inner join accounts on reviews.creator_id = accounts.id where reviews.id = ? and pwhash = ?;', [reviewID, pwhash] , function (err, result, fields) {
